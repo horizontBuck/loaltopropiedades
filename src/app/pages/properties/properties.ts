38b433ce46ebbe65a88a8be003aa85d3b477fda3
bunsen <bunsen@bunsen.bunsen>
2025-10-29 08:28:53 -0500

    arreglos español

--- src/app/pages/properties/properties.ts
+++ src/app/pages/properties/properties.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-properties',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './properties.html',
   styleUrl: './properties.scss',
 })
 export class Properties {
-
+  constructor(private router: Router) {}
+  
+ navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
 }
